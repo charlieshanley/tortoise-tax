1fc58e9da8d792d532f099cec411e60c4ec6143a
Charlie Hanley <charles.scott.hanley+git@gmail.com>
2022-12-08 23:06:15 -0500

    Simplest possible scenario comparison via NonEmpty

--- tortoise-tax-cli/app/Main.hs
+++ tortoise-tax-cli/app/Main.hs
@@ -4,8 +4,9 @@ module Main where
 
 import Prelude hiding (Ap)
 
+import           Data.List.NonEmpty (NonEmpty(..))
 import qualified Data.Text.IO as Text
-import Data.Text.Read (decimal, signed)
+import           Data.Text.Read (decimal, signed)
 import qualified TaxCode.Example
 import           TortoiseTax
 
@@ -14,16 +15,22 @@ main = do
     putStrLn "Hello, TortoiseTax!"
     taxSituation <- interview TaxCode.Example.income
     putStrLn "Your income:"
-    putStrLn . show . runIdentity $ eval taxSituation
+    putStrLn . show $ eval taxSituation
 
-interview :: TaxCode a -> IO (TaxSituation a)
+interview :: TaxCode a -> IO (TaxSituations a)
 interview = \case
-  Lit (Info name simpleExplanation) (Q question fromAnswer) -> do
-      putStrLn ""
+  Pure (Info name simpleExplanation) (Q question fromAnswer) -> do
+      putStrLn "-----"
       Text.putStrLn name
       traverse_ Text.putStrLn simpleExplanation
       Text.putStrLn question
-      Right input <- fromAnswer <$> getLine
-      pure $ Lit (Info name simpleExplanation) (Identity input)
-  F mInfo f a -> F mInfo f <$> interview a
+      Right head <- fromAnswer <$> getLine
+      let next = do
+            input <- getLine
+            case fromAnswer input of
+              Left err -> pure []
+              Right x -> (:) x <$> next
+      tail <- next
+      pure $ Pure (Info name simpleExplanation) (head :| tail)
+  Fmap mInfo f a -> Fmap mInfo f <$> interview a
   Ap mInfo f a -> Ap mInfo <$> interview f <*> interview a
--- tortoise-tax/src/TortoiseTax.hs
+++ tortoise-tax/src/TortoiseTax.hs
@@ -6,15 +6,18 @@ module TortoiseTax where
 import Data.Functor.Identity (Identity)
 import Data.Text             (Text)
 import Data.Text.Read        (signed, decimal)
+import Data.List.NonEmpty    (NonEmpty)
 
 type TaxCode = Expr Question
 
 type TaxSituation = Expr Identity
 
+type TaxSituations = Expr NonEmpty
+
 data Expr f a where
-    Lit :: Info -> f a -> Expr f a
-    F   :: Maybe Info -> (a -> b) -> Expr f a -> Expr f b
-    Ap  :: Maybe Info -> Expr f (a -> b) -> Expr f a -> Expr f b
+    Pure :: Info -> f a -> Expr f a
+    Fmap :: Maybe Info -> (a -> b) -> Expr f a -> Expr f b
+    Ap   :: Maybe Info -> Expr f (a -> b) -> Expr f a -> Expr f b
 
 data Question a = Q
     { question :: Text
@@ -30,10 +33,10 @@ data Info = Info
     }
 
 int :: (Num a, Integral a) => Info -> Text -> TaxCode a
-int info questionText = Lit info $ Q questionText $ fmap fst . signed decimal
+int info questionText = Pure info $ Q questionText $ fmap fst . signed decimal
 
 f2 :: (a -> b -> c) -> Maybe Info -> Expr f a -> Expr f b -> Expr f c
-f2 f mInfo a b = Ap mInfo (F Nothing f a) b
+f2 f mInfo a b = Ap mInfo (Fmap Nothing f a) b
 
 add :: ( Num a ) => Maybe Info -> Expr f a -> Expr f a -> Expr f a
 add = f2 (+)
@@ -43,6 +46,6 @@ subtr = f2 (-)
 
 
 eval :: ( Applicative f ) => Expr f a -> f a
-eval (Lit _ fa) = fa
-eval (F _ f a)    = f <$> eval a
+eval (Pure _ fa) = fa
+eval (Fmap _ f a)    = f <$> eval a
 eval (Ap _ f a)   = eval f <*> eval a
