fcbf21304c1d15002fa8f8ce42c2db48ca7eb78a
Charlie Hanley <charles.scott.hanley+git@gmail.com>
2022-12-15 20:24:23 -0500

    Add Num instance

--- tortoise-tax/src/TaxCode/Example.hs
+++ tortoise-tax/src/TaxCode/Example.hs
@@ -6,14 +6,14 @@ module TaxCode.Example where
 import           TortoiseTax
 
 totalTax :: Interview Int
-totalTax = add (Just $ Info "Total tax" Nothing) incomeTax capitalGainsTax
+totalTax = add (Info "Total tax" Nothing) incomeTax capitalGainsTax
   where
     incomeTax = round . (*) (0.25 :: Float) . fromIntegral <$> income
-    income = add (Just $ Info "Total income" Nothing) wages taxableInterest
+    income = add (Info "Total income" Nothing) wages taxableInterest
     wages = int @Int (Info "Wages" (Just "Wages, salaries, and tips"))
         "How much did you earn as wage income?"
     taxableInterest = subtr
-        (Just $ Info "Taxable interest" $ Just "This is the interest you received that is subject to tax")
+        (Info "Taxable interest" $ Just "This is the interest you received that is subject to tax")
         interest
         nontaxableInterest
     interest = int (Info "Interest" (Just "Interest you received from all sources"))
@@ -21,6 +21,6 @@ totalTax = add (Just $ Info "Total tax" Nothing) incomeTax capitalGainsTax
     nontaxableInterest = int (Info "Nontaxable interest" Nothing)
         "How much interest did you receive from nontaxable sources, such as municipal bonds?"
     capitalGainsTax = round . (*) (0.15 :: Float) . fromIntegral <$> netCapitalGains
-    netCapitalGains = max 0 <$> subtr Nothing capitalGains capitalLosses
+    netCapitalGains = max 0 <$> capitalGains - capitalLosses
     capitalLosses = int (Info "Capital losses" Nothing) "What were your gross capital losses?"
     capitalGains = int @Int (Info "Capital gains" Nothing) "What were your gross capital gains?"
--- tortoise-tax/src/TortoiseTax.hs
+++ tortoise-tax/src/TortoiseTax.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE TypeFamilies  #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeOperators     #-}
 
 module TortoiseTax where
 
+import           Control.Applicative
 import           Control.Applicative.Free
 import           Data.Functor.Compose
 import           Data.Functor.Identity
@@ -38,17 +39,26 @@ int :: (Num a, Integral a) => Info -> Text -> Interview a
 int info questionText = liftAp $
     Compose (Just info, InL $ Q questionText $ fmap fst . signed decimal)
 
-f2 :: (a -> b -> c) -> Maybe Info -> Interview a -> Interview b -> Interview c
-f2 f mInfo a b = liftAp (Compose (mInfo, InR $ Identity f)) <*> a <*> b
+f2 :: (a -> b -> c) -> Info -> Interview a -> Interview b -> Interview c
+f2 f info a b = liftAp (Compose (Just info, InR $ Identity f)) <*> a <*> b
 
-add :: ( Num a ) => Maybe Info -> Interview a -> Interview a -> Interview a
+add :: ( Num a ) => Info -> Interview a -> Interview a -> Interview a
 add = f2 (+)
 
-subtr :: ( Num a ) => Maybe Info -> Interview a -> Interview a -> Interview a
+subtr :: ( Num a ) => Info -> Interview a -> Interview a -> Interview a
 subtr = f2 (-)
 
-mult :: ( Num a ) => Maybe Info -> Interview a -> Interview a -> Interview a
+mult :: ( Num a ) => Info -> Interview a -> Interview a -> Interview a
 mult = f2 (*)
 
 eval :: (Applicative f) => Ap (TaxField . f) x -> f x
 eval = runAp $ snd . getCompose
+
+instance Num a => Num (Interview a) where
+    (+) = liftA2 (+)
+    (-) = liftA2 (-)
+    (*) = liftA2 (*)
+    negate = fmap negate
+    abs = fmap abs
+    signum = fmap signum
+    fromInteger = pure . fromInteger
