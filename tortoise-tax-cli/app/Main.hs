51e3712a58078c4f9927a7b86b20129ab40becb3
Charlie Hanley <charles.scott.hanley+git@gmail.com>
2022-12-15 23:31:40 -0500

    Switch to expression type that only permits numeric ops (and only carries one leaf type), and get automatic differentitation goint!

--- tortoise-tax-cli/app/Main.hs
+++ tortoise-tax-cli/app/Main.hs
@@ -1,12 +1,13 @@
+{-# LANGUAGE LambdaCase    #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Main where
 
-import           Prelude                  hiding (Ap, Sum)
+import           Prelude          hiding (Ap, Sum)
 
-import           Control.Applicative.Free
 import           Data.Functor.Sum
-import qualified Data.Text.IO             as Text
+import qualified Data.Text.IO     as Text
+import qualified Numeric.AD       as AD
 import qualified TaxCode.Example
 import           TortoiseTax
 
@@ -15,27 +16,35 @@ main = do
     putStrLn "Hello, TortoiseTax!"
     taxSituation <- interview TaxCode.Example.totalTax
     putStrLn "Your stuff:"
-    traverse_ Text.putStrLn $ printAp taxSituation
+    print taxSituation
+    putStrLn "Your marginal rates:"
+    print $ AD.grad (runIdentity . eval) taxSituation
     putStrLn "Your total tax:"
     print $ eval taxSituation
 
-interview :: Interview a -> IO (TaxSituations a)
-interview = getCompose . runAp askQuestion
+interview :: Interview a -> IO (TaxSituation a)
+interview = run askQuestion
 
-askQuestion :: (TaxField . Sum Question Identity) x -> (IO . TaxSituations) x
-askQuestion (Compose (mInfo, value)) = Compose $
-    case value of
-      InR (Identity x) -> pure $ liftAp $ Compose (mInfo, pure x)
-      InL (Q questionText fromAnswer) -> do
-          putStrLn "-----"
-          traverse_ (Text.putStrLn . name) mInfo
-          traverse_ Text.putStrLn $ simpleExplanation =<< mInfo
-          Text.putStrLn questionText
-          Right firstInput <- fromAnswer <$> getLine
-          let next = do
-                input <- getLine
-                case fromAnswer input of
-                  Left _  -> pure []
-                  Right x -> (:) x <$> next
-          moreInputs <- next
-          pure $ liftAp $ Compose (mInfo, firstInput :| moreInputs)
+askQuestion :: (Sum Question Identity) a -> IO (Identity a)
+askQuestion = \case
+    InR (Identity x) -> pure $ pure x
+    InL (Q questionText fromAnswer) -> do
+        putStrLn "-----"
+        Text.putStrLn questionText
+        Right input <- fromAnswer <$> getLine
+        pure $ pure input
+
+askQuestionNonEmpty :: (Sum Question Identity) a -> IO (NonEmpty a)
+askQuestionNonEmpty = \case
+    InR (Identity x) -> pure $ pure x
+    InL (Q questionText fromAnswer) -> do
+        putStrLn "-----"
+        Text.putStrLn questionText
+        Right firstInput <- fromAnswer <$> getLine
+        let next = do
+              input <- getLine
+              case fromAnswer input of
+                Left _  -> pure []
+                Right x -> (:) x <$> next
+        moreInputs <- next
+        pure $ firstInput :| moreInputs
--- tortoise-tax/src/TortoiseTax.hs
+++ tortoise-tax/src/TortoiseTax.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE DeriveFoldable        #-}
+{-# LANGUAGE DeriveFunctor         #-}
+{-# LANGUAGE DeriveTraversable     #-}
+{-# LANGUAGE DerivingStrategies    #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE LambdaCase            #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -7,29 +11,50 @@
 
 module TortoiseTax where
 
-import           Control.Applicative
-import           Control.Applicative.Free
 import           Data.Functor.Compose
 import           Data.Functor.Identity
 import           Data.Functor.Sum
-import           Data.List.NonEmpty       (NonEmpty)
-import           Data.Text                (Text)
-import           Data.Text.Read           (double, signed)
+import           Data.List.NonEmpty    (NonEmpty)
+import           Data.Text             (Text)
+import           Data.Text.Read        (double, signed)
+
+data Expr f a
+    = Leaf (Maybe Info) (f a)
+    | Unary UnaryOp (Maybe Info) (Expr f a)
+    | Binary BinaryOp (Maybe Info) (Expr f a) (Expr f a)
+    deriving stock (Show, Functor, Foldable, Traversable)
+
+data UnaryOp = Negate | Abs | Signum | Recip
+    deriving (Show)
+
+getUnaryOp :: ( Fractional a ) => UnaryOp -> a -> a
+getUnaryOp = \case
+    Negate -> negate
+    Abs    -> abs
+    Signum -> signum
+    Recip  -> recip
+
+data BinaryOp = Add | Subtr | Mult | Div
+    deriving (Show)
+
+getBinaryOp :: ( Fractional a ) => BinaryOp -> a -> a -> a
+getBinaryOp = \case
+    Add   -> (+)
+    Subtr -> (-)
+    Mult  -> (*)
+    Div   -> (/)
 
 type (.) = Compose
 
-type TaxField = (,) (Maybe Info)
-
-type Interview = Ap (TaxField . Sum Question Identity)
-
-type TaxSituation = Ap (TaxField . Identity)
-
-type TaxSituations = Ap (TaxField . NonEmpty)
+type Interview     = Expr (Sum Question Identity)
+type TaxSituation  = Expr Identity
+type TaxSituations = Expr NonEmpty
 
 data Question a = Q
     { qText       :: Text
     , qFromAnswer :: Text -> Either String a
     }
+    deriving stock (Functor)
 
 data Info = Info
     { name              :: Text
@@ -38,41 +63,50 @@ data Info = Info
     -- TODO , mdInstruction :: Maybe InstructionRef
     -- TODO , mdFormField :: Maybe FormFieldRef
     }
+    deriving (Show)
 
 dbl :: Info -> Text -> Interview Double
-dbl info questionText = liftAp $
-    Compose (Just info, InL $ Q questionText $ fmap fst . signed double)
+dbl info questionText = Leaf (Just info) $
+    InL $ Q questionText $ fmap fst . signed double
 
-f2 :: (a -> b -> c) -> Info -> Interview a -> Interview b -> Interview c
-f2 f info a b = liftAp (Compose (Just info, InR $ Identity f)) <*> a <*> b
+add :: Info -> Expr f a -> Expr f a -> Expr f a
+add = Binary Add . Just
 
-add :: ( Num a ) => Info -> Interview a -> Interview a -> Interview a
-add = f2 (+)
+subtr :: Info -> Interview a -> Interview a -> Interview a
+subtr = Binary Subtr . Just
 
-subtr :: ( Num a ) => Info -> Interview a -> Interview a -> Interview a
-subtr = f2 (-)
+mult :: Info -> Interview a -> Interview a -> Interview a
+mult = Binary Mult . Just
 
-mult :: ( Num a ) => Info -> Interview a -> Interview a -> Interview a
-mult = f2 (*)
+div :: Info -> Interview a -> Interview a -> Interview a
+div = Binary Div . Just
 
-eval :: (Applicative f) => Ap (TaxField . f) x -> f x
-eval = runAp $ snd . getCompose
-
-printAp :: Ap (TaxField . f) a -> [ Text ]
-printAp = runAp_ $ \case
-    Compose (Just (Info nm _), _) -> [ nm ]
-    Compose (Nothing, _)          -> [ "--" ]
-
-instance Num a => Num (Interview a) where
-    (+) = liftA2 (+)
-    (-) = liftA2 (-)
-    (*) = liftA2 (*)
-    negate = fmap negate
-    abs = fmap abs
-    signum = fmap signum
-    fromInteger = pure . fromInteger
+instance ( Num a ) => Num (Interview a) where
+    (+) = Binary Add Nothing
+    (-) = Binary Subtr Nothing
+    (*) = Binary Mult Nothing
+    negate = Unary Negate Nothing
+    abs = Unary Abs Nothing
+    signum = Unary Signum Nothing
+    fromInteger = Leaf Nothing . InR . Identity . fromInteger
 
 instance Fractional a => Fractional (Interview a) where
-    (/) = liftA2 (/)
-    recip = fmap recip
-    fromRational = pure . fromRational
+    (/) = Binary Div Nothing
+    recip = Unary Recip Nothing
+    fromRational = Leaf Nothing . InR . Identity . fromRational
+
+run :: ( Applicative m ) => (f a -> m (g a)) -> Expr f a -> m (Expr g a)
+run f = \case
+    Leaf mInfo fa       -> Leaf mInfo <$> f fa
+    Unary op mInfo a    -> Unary op mInfo <$> run f a
+    Binary op mInfo a b -> Binary op mInfo <$> run f a <*> run f b
+
+eval :: ( Applicative f, Fractional a ) => Expr f a -> f a
+eval = \case
+    Leaf _ fa       -> fa
+    Unary op _ a    -> getUnaryOp op <$> eval a
+    Binary op _ a b -> getBinaryOp op <$> eval a <*> eval b
+
+
+-- instance Show (f a) => Show (Expr f a) where
+--     show (Leaf
